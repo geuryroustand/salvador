3d7679b5aa040d8096c67119b4689058a0cd794f
Geury Roustand <61476699+geuryroustand@users.noreply.github.com>
2023-02-08 17:21:22 +0100

    added all the files again

---
+++ salvador/apiSecret.swift
@@ -0,0 +1,10 @@
+//
+//  apiSecret.swift
+//  salvador
+//
+//  Created by Geury Roustand on 07.02.23.
+//
+
+import Foundation
+
+var apiSecret = "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"
