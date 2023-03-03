c41251f2824b554b2af51c87e8d3ddc1fe68d043
Geury Roustand <61476699+geuryroustand@users.noreply.github.com>
2023-03-03 09:40:54 +0100

    feat: added ci_scripts folder

---
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -0,0 +1,17 @@
+
+#!/bin/sh
+
+echo "Stage: PRE-Xcode Build is activated .... "
+
+# Move to the place where the scripts are located.
+# This is important because the position of the subsequently mentioned files depend of this origin.
+cd $CI_WORKSPACE/ci_scripts || exit 1
+
+# Write a JSON File containing all the environment variables and secrets.
+printf "{\"APISecret\":\"%s\"}" "$APISecret" >> ../salvador/Preview Content/APISecret.json
+
+echo "Wrote Secrets.json file."
+
+echo "Stage: PRE-Xcode Build is DONE .... "
+
+exit 0
